fefe5fd9ae2a83056b4f66534bc1d2b0e5023f41
cymon1997 <cybermonster82@yahoo.com>
2019-04-07 20:23:40 +0700

    added modular layout

---
+++ app/src/main/java/com/cymonevo/aurora/template/layout/HeaderFragment.java
@@ -0,0 +1,21 @@
+package com.cymonevo.aurora.template.layout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cymonevo.aurora.template.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HeaderFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_header, container, false);
+        return root;
+    }
+}

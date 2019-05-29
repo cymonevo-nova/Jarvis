15888ebdd15b9c2cd78277f5bfae32e69e017723
cymon1997 <cybermonster82@yahoo.com>
2019-05-29 14:54:28 +0700

    added base splash screen

---
+++ app/src/main/java/com/cymonevo/nova/template/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.cymonevo.nova.template;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.cymonevo.nova.template.config.Config;
+import com.cymonevo.nova.template.core.Router;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_splash);
+        Thread loading = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    finishLoading();
+                } finally {
+                    finishLoading();
+                }
+            }
+        };
+        loading.start();
+    }
+
+    private void finishLoading() {
+        if (Config.DEBUG_MODE) {
+            Router.gotoTest(this);
+        } else {
+            Router.gotoMain(this);
+        }
+        finish();
+    }
+}

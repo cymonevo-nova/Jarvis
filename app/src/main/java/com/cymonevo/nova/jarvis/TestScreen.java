a01693e986065837da062634181ce7ef331d95ed
cymon1997 <cybermonster82@yahoo.com>
2020-01-13 20:16:15 +0700

    install pocketsphinx

--- app/src/main/java/com/cymonevo/nova/jarvis/TestScreen.java
+++ app/src/main/java/com/cymonevo/nova/jarvis/TestScreen.java
@@ -10,20 +10,27 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cymonevo.nova.jarvis.core.Router;
-import com.cymonevo.nova.jarvis.service.Provider;
 import com.cymonevo.nova.jarvis.service.api.APICall;
 import com.cymonevo.nova.jarvis.service.api.APIResponse;
 import com.cymonevo.nova.jarvis.service.db.DBCall;
 import com.cymonevo.nova.jarvis.service.db.user.DBResult;
 import com.cymonevo.nova.jarvis.service.db.user.entity.UserData;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import edu.cmu.pocketsphinx.Assets;
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
+import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
 
-public class TestScreen extends AppCompatActivity implements APICall, DBCall {
+public class TestScreen extends AppCompatActivity implements APICall, DBCall, RecognitionListener {
     @BindView(R2.id.tv_test)
     TextView tvTest;
     private static final int DB_LIST_USER_CODE = 10;
@@ -43,6 +50,44 @@ public class TestScreen extends AppCompatActivity implements APICall, DBCall {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    private void sphinx() throws IOException {
+        File asset = new Assets(this).syncAssets();
+        SpeechRecognizer recognizer = SpeechRecognizerSetup.defaultSetup().setAcousticModel(new File(asset, "en-us-ptm"))
+                .setDictionary(new File(asset, "cmudict-en-us.dict")).getRecognizer();
+        recognizer.addListener(this);
+        recognizer.addKeyphraseSearch("wakeup", "oh god");
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onPartialResult(Hypothesis hypothesis) {
+
+    }
+
+    @Override
+    public void onResult(Hypothesis hypothesis) {
+
+    }
+
+    @Override
+    public void onError(Exception e) {
+
+    }
+
+    @Override
+    public void onTimeout() {
+
     }
 
     @Override
